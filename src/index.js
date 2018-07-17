54dc704f7ae726fcfde1db0e0ce9bba4c048b7b1
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-07-17 16:33:39 +0300

    add redux
    - add redux and support libs
    - basic store setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    createStore,
+    combineReducers
+} from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import '../node_modules/semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(combineReducers({
+    // TODO: add reducers
+}));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
