c35500e1da5216e3b0be54f50ddf6890325a745d
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-07-23 11:56:09 +0300

    add Queue container

---
+++ src/containers/Queue.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Queue extends Component {
+    render() {
+        return <p>Queue container</p>
+    }
+}
+
+export default withRouter(Queue);
