31a5894f839d0f3f0fa021fd5a565f1e4bc1b259
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-07-23 11:56:39 +0300

    add route to Queue container

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Route } from 'react-router-dom';
 import App from './App';
+import Queue from './Queue';
 
 const Root = ({ store }) => (
     <Provider store={store}>
         <div className="Root">
-            <Route path="/" component={App} />
+            <Route path="/" exact component={App} />
+            <Route path="/queue/:queueName" exact component={Queue} />
         </div>
     </Provider>
 );
