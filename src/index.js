3df72265480a85e5da107b49d86a1170b91885cf
Oleksii Lystopadov <oleksiily@softwareplanetgroup.com>
2018-07-25 11:44:15 +0300

    remove semantic styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import Root from './containers/Root';
-import '../node_modules/semantic-ui-css/semantic.min.css';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
