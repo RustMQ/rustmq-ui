7f7737455c461d82fa901beb7cae8969de52b26e
Oleksii Lystopadov <oleksiily@softwareplanetgroup.com>
2018-07-31 11:29:12 +0300

    add Modal to Root component

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route } from 'react-router-dom';
+import ReactModal from 'react-modal';
 import App from './App';
 import Queue from './Queue';
 import NewQueueForm from '../components/NewQueueForm/NewQueueForm';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import NewMessageForm from '../components/NewMessageForm/NewMessageForm'
 import './Root.css';
+
+ReactModal.setAppElement('#root');
 
 const Root = ({ store }) => (
     <Provider store={store}>
-        <div className="root">
+        <div id="root" className="root">
             <Header />
             <main className="root__main">
                 <Route path="/" exact component={App} />
                 <Route path="/queue/:queueName" exact component={Queue} />
                 <Route path="/queues/new" exact component={NewQueueForm} />
             </main>
+            <ReactModal className='modal' overlayClassName="modal-overlay" ><NewMessageForm /></ReactModal>
             <Footer />
         </div>
     </Provider>
