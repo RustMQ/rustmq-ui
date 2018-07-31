31c13484a035b3ef16a2d92d77ea82dd6b4be5a8
Oleksii Lystopadov <oleksiily@softwareplanetgroup.com>
2018-07-31 17:41:55 +0300

    remove state dependency
    - new message form is a reference to DOM element

--- src/components/NewMessageForm/NewMessageForm.js
+++ src/components/NewMessageForm/NewMessageForm.js
@@ -8,34 +8,28 @@ class NewMessageForm extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            body: ''
-        };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleClose = this.handleClose.bind(this);
     }
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
     async handleSubmit(event) {
         event.preventDefault();
+        const { body } = this.form;
         const { queueName } = this.props;
-        await this.props.postMessage(queueName, this.state);
+        //TODO introduce default message constructor
+        const message = {
+            body: body.value,
+            timeout: 60,
+            delay: 0,
+            expiration: 604800
+        };
+        await this.props.postMessage(queueName, message);
         await this.props.loadQueue(queueName);
         this.props.hideModal();
     }
 
-    handleClose(event) {
+    handleClose() {
         this.props.hideModal();
     }
 
@@ -43,27 +37,25 @@ class NewMessageForm extends Component {
         return (
             <div className="new-message-form__modal">
                 <Button onClick={this.handleClose} class="button button--close new-message-form__modal__close-button" />
-                <form onSubmit={this.handleSubmit}>
+                <form ref={form => this.form = form} onSubmit={this.handleSubmit}>
                     <div className="new-message-form__modal__header">Send a New Message</div>
                     <div className="new-message-form__modal__controls">
                         <textarea
                             className="new-message-form__modal__controls__text-area"
                             name="body"
                             placeholder="Your message..."
-                            value={this.state.body}
-                            onChange={this.handleInputChange}
                         ></textarea>
                         <div className="new-message-form__modal__control">
                             <label className="new-message-form__modal__control__label" htmlFor="timeout">Timeout</label>
-                            <input className="new-message-form__modal__control__input" placeholder="60 sec by default" id="timeout" type="text" />
+                            <input className="new-message-form__modal__control__input" placeholder="60 sec by default" id="timeout" name="timeout" type="text" />
                         </div>
                         <div className="new-message-form__modal__control">
                             <label className="new-message-form__modal__control__label" htmlFor="delay">Delay</label>
-                            <input className="new-message-form__modal__control__input" placeholder="0 sec by default" id="delay" type="text" />
+                            <input className="new-message-form__modal__control__input" placeholder="0 sec by default" id="delay" name="delay" type="text" />
                         </div>
                         <div className="new-message-form__modal__control">
                             <label className="new-message-form__modal__control__label" htmlFor="expiration">Expiration</label>
-                            <input className="new-message-form__modal__control__input" placeholder="604,800 sec (7 days) by default" id="expiration" type="text" />
+                            <input className="new-message-form__modal__control__input" placeholder="604,800 sec (7 days) by default" id="expiration" name="expiration" type="text" />
                         </div>
                     </div>
                     <div className="new-message-form__modal__buttons">
