f019aeeb06c9a04d9de1fe6517fefc224326ccf1
Oleksii Lystopadov <oleksiily@softwareplanetgroup.com>
2018-08-14 09:15:22 +0300

    add confirmation dialog component

---
+++ src/components/ConfirmationDialog/ConfirmationDialog.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { hideModal } from '../../actions';
+import Button from '../Button/Button';
+import './ConfirmationDialog.css';
+
+class ConfirmationDialog extends Component {
+    render() {
+        return (
+            <div className="confirmation-dialog">
+                <div className="confirmation-dialog__title">{this.props.title}</div>
+                <div>{this.props.message}</div>
+                <div className="confirmation-dialog__controls">
+                    <Button onClick={this.props.handleConfirm} class="button button--send confirmation-dialog__controls__button"  label="Ok" />
+                    <Button onClick={() => this.props.hideModal()} class="button button--send confirmation-dialog__controls__button" label="Cancel" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default connect(null, { hideModal })(ConfirmationDialog);
